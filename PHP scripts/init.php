7a524cc6b8dc982b830b7d8e7d736627c9a5e194
Kirsten Magnus T. Untal <kirsten.untal@obf.ateneo.edu>
2018-11-15 20:30:15 +0800

    Create init.php

---
+++ PHP scripts/init.php
@@ -0,0 +1,9 @@
+<?php
+$db_name="saankakain";
+$mysql_user="*insert user here*";
+$mysql_pass="*insert password here*";
+$server_name="localhost";
+
+$con=mysqli_connect($server_name,$mysql_user,$mysql_pass,$db_name);
+
+?>
