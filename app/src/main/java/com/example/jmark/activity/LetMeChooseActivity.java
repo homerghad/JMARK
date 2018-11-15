42bf48266b4525f0fe7a9284e73f8323c2db2c14
Homer <jamesreotutar@gmail.com>
2018-11-15 19:50:16 +0800

    local changes

--- app/src/main/java/com/example/jmark/activity/LetMeChooseActivity.java
+++ app/src/main/java/com/example/jmark/activity/LetMeChooseActivity.java
@@ -1,21 +1,43 @@
 package com.example.jmark.activity;
 
 import android.content.Intent;
+import android.databinding.DataBinderMapperImpl;
+import android.databinding.DataBindingComponent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.jmark.R;
+import com.example.jmark.databinding.ActivityLetMeChooseBinding;
+import com.example.jmark.dataobject.Answer;
+import com.example.jmark.dataobject.Question;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LetMeChooseActivity extends AppCompatActivity {
+
+    private Question question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_let_me_choose);
+
+        ActivityLetMeChooseBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_let_me_choose);
+
+
+        question = new Question();
+        question.setText("How much are you willing to spend?");
+
+        binding.setQuestion(question);
+
+//        setContentView(R.layout.activity_let_me_choose);
     }
 
     public void goToChosenRestaurantActivity(View view) {
+
+
         Intent intent = new Intent(this, ChooseForMeActivity.class);
         /*EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
---
+++ app/src/main/java/com/example/jmark/dataobject/Answer.java
@@ -0,0 +1,22 @@
+package com.example.jmark.dataobject;
+
+public class Answer {
+    private String id;
+    private String text;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
