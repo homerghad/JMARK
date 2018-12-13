3311fd3c3f94a912e194f8c9fa6d85ea94939661
Kristi Ingco <kristi.ingco@gmail.com>
2018-12-13 18:17:12 +0800

    Added gif and changed layout of chosen restaurants

--- app/src/main/java/com/example/jmark/activity/QuestionThreeActivity.java
+++ app/src/main/java/com/example/jmark/activity/QuestionThreeActivity.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 
 public class QuestionThreeActivity extends Activity {
     public static final String MY_PREFS_NAME = "MyPrefsFile";
-    private static final String URL = "foodfindconsole.herokuapp.com/";
+    private static final String URL = "https://foodfindconsole.herokuapp.com/";
     private static final String QuestionURL = "question_script.php";
     private static final String AnswerURL = "answer_script.php";
     JSONParser jsonParser = new JSONParser();
